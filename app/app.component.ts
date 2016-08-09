d718f401f278ad82cea5019cd8db140a3e5da42b
Deepak Rawat <deepak.rawat@oodlestechnologies.com>
2016-08-09 18:02:45 +0530

    Data binding and template rendering

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,11 +10,11 @@ export class Hero{
   template: `<h1>{{title}}</h1>
              <h2>My Heroes</h2>
              <ul class="heroes">
-                <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+                <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
                     <span class="badge">{{hero.id}}</span> {{hero.name}}
                 </li>
              </ul>
-             <div *ngIF="selectedHero">
+             <div *ngIf="selectedHero">
                  <h2>{{selectedHero.name}} details!</h2>
                  <div><label>id: </label>{{selectedHero.id}}</div>
                  <div>
@@ -76,8 +76,10 @@ export class Hero{
 
 export class AppComponent {
     title = "Tour of Heroes";
+    selectedHero: Hero;
     public heroes = HEROES;
-    onSelect(selectedHero: Hero){
+    onSelect(hero: Hero){
+        this.selectedHero = hero;
     }
 }
 
